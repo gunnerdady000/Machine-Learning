110e033424b1917e667c0de0922f71ab4be521b5
Micah <56775725+gunnerdady000@users.noreply.github.com>
2020-12-06 12:50:59 -0700

    Fixed SK's Error Output

--- Machine Learning/Logistic Regression/test_LogisticRegression.py
+++ Machine Learning/Logistic Regression/test_LogisticRegression.py
@@ -10,6 +10,7 @@ def test_logistic():
     iris = datasets.load_iris()
     x = iris.data[:, :2]
     y = (iris.target != 0) * 1
+    y_sk = y
     y = y[:, np.newaxis]
     logic = LogisticLearner(0.01, 10000)
     logic.fit(x, y)
@@ -17,7 +18,7 @@ def test_logistic():
     print(logic.weights)
     print(logic.bias)
     print(f"Accuracy: ", logic.accuracy(y))
-    clf = LogisticRegression(random_state=0).fit(x, y)
+    clf = LogisticRegression(random_state=0).fit(x, y_sk)
     clf.predict(x[:2, :])
     print(clf.score(x, y))
     logic.graph(x, y)
